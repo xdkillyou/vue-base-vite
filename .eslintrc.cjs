ffc5902d6fe8ad6ed5cdba41d91fb97405cbbc4c
xdkillyou <xdkillyou@gmail.com>
2023-11-18 11:48:12 +0800

    chore: add prettier & update Eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,19 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    // 'eslint:recommended',
+    // '@vue/eslint-config-typescript'
+    '@vue/eslint-config-airbnb-with-typescript',
+    'plugin:prettier/recommended',
   ],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+};
